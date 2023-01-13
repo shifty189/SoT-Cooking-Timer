4d04b276af98989c92add56db7477d78fc4906d1
shifty189 <shifty189@gmail.com>
2023-01-13 18:37:33 -0500

    Update Vails.py

--- Vails.py
+++ Vails.py
@@ -8,6 +8,67 @@ def clearWindow(window):
         widget.destroy()
 
 
+def timeDisplay(window, list):
+    """the list argument is only for a global list to store the image file.
+    This is to prevent trash collection from removing the image before its used"""
+    clearWindow(window)
+    infoFrame = tk.Frame(window)
+    infoFrame.pack()
+    infoLabel = tk.Label(window,
+                         text="Sailor’s Bounty, Highest point of the mountian by some cooking pots")
+    infoLabel.pack(pady=3)
+    image = tk.PhotoImage(file="time.gif")
+    list.append(image)
+    imageLabel = tk.Label(window, image=image)
+    imageLabel.pack()
+
+
+def cursedskullsDisplay(window, list):
+    """the list argument is only for a global list to store the image file.
+            This is to prevent trash collection from removing the image before its used"""
+    clearWindow(window)
+    infoFrame = tk.Frame(window)
+    infoFrame.pack()
+    infoLabel = tk.Label(window,
+                         text="Sailor’s Bounty, Southeast beach of the main island by a pile of skulls")
+    infoLabel.pack(pady=3)
+    image = tk.PhotoImage(file="cursedskulls.gif")
+    list.append(image)
+    imageLabel = tk.Label(window, image=image)
+    imageLabel.pack()
+
+
+def knowledgeispowerDisplay(window, list):
+    """the list argument is only for a global list to store the image file.
+        This is to prevent trash collection from removing the image before its used"""
+    clearWindow(window)
+    infoFrame = tk.Frame(window)
+    infoFrame.pack()
+    infoLabel = tk.Label(window,
+                         text="Wanderer’s Refuge, North beach by stone foundations")
+    infoLabel.pack(pady=3)
+    # image = tk.PhotoImage(file="olsudds.gif")
+    # list.append(image)
+    # imageLabel = tk.Label(window, image=image)
+    # imageLabel.pack()
+
+
+def olSuddsDisplay(window, list):
+    """the list argument is only for a global list to store the image file.
+    This is to prevent trash collection from removing the image before its used
+    TODO: add image"""
+    clearWindow(window)
+    infoFrame = tk.Frame(window)
+    infoFrame.pack()
+    infoLabel = tk.Label(window,
+                         text="Wanderer’s Refuge, South beach by a collasped building")
+    infoLabel.pack(pady=3)
+    image = tk.PhotoImage(file="olsudds.gif")
+    list.append(image)
+    imageLabel = tk.Label(window, image=image)
+    imageLabel.pack()
+
+
 def anchientMap(window, list):
     """the list argument is only for a global list to store the image file.
         This is to prevent trash collection from removing the image before its used"""
@@ -99,3 +160,27 @@ def find_sudds(window, list):
                                    command=lambda x = window: anchientMap(x, list)
                                    )
     achient_map_button.pack(pady=5)
+    ol_sudds_text = "Not all who wander find what they seek, but ol' Sudds did, oh yes he did. Come to the south beach of Wanderer's Refuge and I'll tell you more"
+    ol_sudds_button = tk.Button(location_frame,
+                                text = ol_sudds_text,
+                                command=lambda x = window: olSuddsDisplay(x, list)
+                                )
+    ol_sudds_button.pack(pady=5)
+    knowledgeispower_text = "Knowledge is powerful, too powerful in the wrong hands. Come to Wanderer's Refuge. At the ruins we will protect the knowledge of the Ancients"
+    knowledgeispower_button = tk.Button(location_frame,
+                                        text= knowledgeispower_text,
+                                        command=lambda x = window: knowledgeispowerDisplay(x, list)
+                                        )
+    knowledgeispower_button.pack(pady=5)
+    cursedskulls_text = "Cursed Skulls, so many curses haunt the Sea of Thieves, but why? The Coral Curse, the first? Sorry. Meet me on the south beach of Sailor's Bounty"
+    cursedskulls_button = tk.Button(location_frame,
+                                    text = cursedskulls_text,
+                                    command=lambda x = window: cursedskullsDisplay(x, list)
+                                    )
+    cursedskulls_button.pack(pady=5)
+    time_text = "Time, it moves so fast. Join me at the camp atop Sailor's Bounty. We'll reach back in time, we will, and find the location of a Veil Stone"
+    time_button = tk.Button(location_frame,
+                            text= time_text,
+                            command=lambda x = window: timeDisplay(x, list)
+                            )
+    time_button.pack(pady=5)
