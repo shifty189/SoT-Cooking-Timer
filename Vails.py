aa5a465de93910a51d46ffa111a61f6b8aff5eea
shifty189 <shifty189@gmail.com>
2023-01-13 07:34:13 -0500

    Update Vails.py

--- Vails.py
+++ Vails.py
@@ -8,6 +8,36 @@ def clearWindow(window):
         widget.destroy()
 
 
+def anchientMap(window, list):
+    """the list argument is only for a global list to store the image file.
+        This is to prevent trash collection from removing the image before its used"""
+    clearWindow(window)
+    infoFrame = tk.Frame(window)
+    infoFrame.pack()
+    infoLabel = tk.Label(window,
+                         text="Smuggler’s Bay, at the large camp in front of Smuggler's cave")
+    infoLabel.pack(pady=3)
+    # image = tk.PhotoImage(file="crescent_shape.gif")
+    # list.append(image)
+    # imageLabel = tk.Label(window, image=image)
+    # imageLabel.pack()
+
+
+def libraryDisplay(window, list):
+    """the list argument is only for a global list to store the image file.
+    This is to prevent trash collection from removing the image before its used
+    TODO: add better image"""
+    clearWindow(window)
+    infoFrame = tk.Frame(window)
+    infoFrame.pack()
+    infoLabel = tk.Label(window, text='at Smuggler’s Bay, by a shaded library to the East')
+    infoLabel.pack(pady=3)
+    image = tk.PhotoImage(file="library.gif")
+    list.append(image)
+    imageLabel = tk.Label(window, image=image)
+    imageLabel.pack()
+
+
 def fiveBarnicleDisplay(window, list):
     """the list argument is only for a global list to store the image file.
         This is to prevent trash collection from removing the image before its used"""
@@ -57,3 +87,15 @@ def find_sudds(window, list):
                                      text=cresant_shape_text,
                                      command=lambda x = window: cresantShapeDisplay(x, list))
     cresant_shape_button.pack(pady=5)
+    library_text = "Libraries are sacred places, repositories of knowledge. Come and find me on Smuggler's Bay, I have Ancient knowledge to impart"
+    library_button = tk.Button(location_frame,
+                               text=library_text,
+                               command=lambda x = window: libraryDisplay(x, list)
+                               )
+    library_button.pack(pady=5)
+    achient_map_text = "The map is finished! The knowledge of the Ancients. Join me at Smuggler's Bay"
+    achient_map_button = tk.Button(location_frame,
+                                   text= achient_map_text,
+                                   command=lambda x = window: anchientMap(x, list)
+                                   )
+    achient_map_button.pack(pady=5)
